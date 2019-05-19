a838793e84b631e71dc83d4a1107c3b822f46b89
davip <davi.p.m94@gmail.com>
2019-05-19 17:02:32 -0300

    mobile update

--- layouts/Partial/Foods.vue
+++ layouts/Partial/Foods.vue
@@ -6,58 +6,70 @@
       <div class="food-content">
         <div class="row">
           <div class="col-md-4">
-            <div class="card">
-              <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+            <nuxt-link to="/" class="card-food-lik">
+              <div class="card">
+                <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                </div>
               </div>
-            </div>
+            </nuxt-link>
           </div>
           <div class="col-md-4">
-            <div class="card">
-              <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+            <nuxt-link to="/" class="card-food-lik">
+              <div class="card">
+                <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                </div>
               </div>
-            </div>
+            </nuxt-link>
           </div>
           <div class="col-md-4">
-            <div class="card">
-              <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+            <nuxt-link to="/" class="card-food-lik">
+              <div class="card">
+                <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                </div>
               </div>
-            </div>
+            </nuxt-link>
           </div>
           <div class="col-md-4">
-            <div class="card">
-              <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+            <nuxt-link to="/" class="card-food-lik">
+              <div class="card">
+                <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                </div>
               </div>
-            </div>
+            </nuxt-link>
           </div>
           <div class="col-md-4">
-            <div class="card">
-              <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+            <nuxt-link to="/" class="card-food-lik">
+              <div class="card">
+                <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                </div>
               </div>
-            </div>
+            </nuxt-link>
           </div>
           <div class="col-md-4">
-            <div class="card">
-              <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
-              <div class="card-body">
-                <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
-                <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+            <nuxt-link to="/" class="card-food-lik">
+              <div class="card">
+                <img src="../../assets/img/img-1.png" alt="" class="img-fluid">
+                <div class="card-body">
+                  <h5 class="card-title">Ham Sandwich <span class="price">$10.50</span></h5>
+                  <p class="card-text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,</p>
+                </div>
               </div>
-            </div>
+            </nuxt-link>
           </div>
         </div>
       </div>
@@ -102,18 +114,42 @@
     border: none;
     margin-bottom: 2rem;
     box-shadow: 0 .5rem 1rem rgba(0,0,0,.15);
+    border-top-right-radius: 20px;
+    border-top-left-radius: 20px;
+    -webkit-transition: all .15s ease-in-out;
+    -moz-transition: all .15s ease-in-out;
+    -ms-transition: all .15s ease-in-out;
+    -o-transition: all .15s ease-in-out;
+    transition: all .15s ease-in-out;
+
+    &-food-lik {
+      color: #AEB2B4;
+      cursor: pointer;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: none;
+      }
+    }
+
     .img-fluid {
       border-top-right-radius: 20px;
       border-top-left-radius: 20px;
     }
+
     &-title {
       display: flex;
       justify-content: space-between;
       align-items: center;
       color: #452268;
     }
+
     .price {
       color: #F98169;
+    }
+
+    &:hover {
+      box-shadow: 0 1rem 3rem rgba(0,0,0,.175);
     }
   }
 </style>
