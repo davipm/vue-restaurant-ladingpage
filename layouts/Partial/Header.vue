fe4841bc660ed2d3a30396aa5ce3c93e8de0c8f2
davip <davi.p.m94@gmail.com>
2019-06-11 11:05:01 -0300

    header update

--- layouts/Partial/Header.vue
+++ layouts/Partial/Header.vue
@@ -2,21 +2,39 @@
   <header class="header">
     <nav class="navbar navbar-expand-lg">
       <div class="container">
-        <a class="navbar-brand" href="#">Victorya</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <nuxt-link class="navbar-brand" to="/">Victorya</nuxt-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-            <li class="nav-item active">
-              <nuxt-link class="nav-link" to="/">Package</nuxt-link>
+            <li
+              class="nav-item"
+              v-for="(item, index) in menus"
+              :key="index"
+            >
+              <nuxt-link
+                class="nav-link"
+                :to="item.link">
+                {{ item.title }}
+              </nuxt-link>
             </li>
+            <!--
             <li class="nav-item">
               <nuxt-link class="nav-link" to="/">About us</nuxt-link>
             </li>
             <li class="nav-item">
               <nuxt-link class="nav-link" to="/">Contact us</nuxt-link>
             </li>
+            -->
           </ul>
         </div>
       </div>
@@ -27,6 +45,15 @@
 <script>
   export default {
     name: 'Header',
+    data() {
+      return {
+        menus: [
+          { title: 'Package', link: '/' },
+          { title: 'About us', link: '/' },
+          { title: 'Contact us', link: '/' },
+        ]
+      }
+    }
   }
 </script>
 
